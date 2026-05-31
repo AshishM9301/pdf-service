89087ce1c30a452ecb4227f14acaeb8ea7fd67c0
AshishM9301 <giftofgodashish@gmail.com>
2026-05-31 08:51:39 +0530

    fixed module bug

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { v4 as uuidv4 } from "uuid";
-import { pushPdfJob, getPdfResult, getQueueInfo, getQueueLength, MAX_PDF_SIZE_BYTES, type PdfJob } from "./redis";
+import { pushPdfJob, getPdfResult, getQueueInfo, getQueueLength, MAX_PDF_SIZE_BYTES, type PdfJob } from "./redis.js";
 import dotenv from "dotenv";
 
 dotenv.config();
